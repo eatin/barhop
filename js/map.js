93d37aac6d5ff588c816dc8f2fca4c6d071a43e3
Tobias Björsberg Åkerlindh <tobias@bjorsberg.se>
2013-10-11 11:29:10 +0200

    Möblerade om kartan

--- js/map.js
+++ js/map.js
@@ -1,5 +1,8 @@
+var map = L.mapbox.map('map', 'examples.map-y7l23tes')
+	.setView([59.314838, 18.069763], 15);
+
 // Set up the foursquare API
-foursquare.settings = {
+var pins = foursquare.settings = {
     /* foursquare API keys. See: https://foursquare.com/oauth/register */
     client_id: 'TBBQCOJODYFMHOV1HSRJSFH4AZI2MV2H1IGDWJTB30J1LYGZ',
     client_secret: 'SNOY0AJ312VC2TH5HHSRUEYYRSJXMCFEKU521FIP4K5UC5Y5',
