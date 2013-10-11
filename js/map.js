f5918ce9ba7d3ad4fa4f22b6193f0a0b3e61bda0
Nathalie Cano <cano.nathalie@gmail.com>
2013-10-11 11:05:53 +0200

    Added map.js (foursquare.js).

---
+++ js/map.js
@@ -0,0 +1,10 @@
+// Set up the foursquare API
+foursquare.settings = {
+    /* foursquare API keys. See: https://foursquare.com/oauth/register */
+    client_id: 'TBBQCOJODYFMHOV1HSRJSFH4AZI2MV2H1IGDWJTB30J1LYGZ',
+    client_secret: 'SNOY0AJ312VC2TH5HHSRUEYYRSJXMCFEKU521FIP4K5UC5Y5',
+    /* List ID. See: https://developer.foursquare.com/docs/explore#req=users/self/lists */
+    list: '5257bc54498e08c238c10737',
+    /* Search radius for nearby address matches */
+    radius: 8046.72 // 5 miles in meters
+};
